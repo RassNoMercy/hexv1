30a4395b57f86f20e08bd6be79a62a899ef70b32
RassNoMercy <107943440+RassNoMercy@users.noreply.github.com>
2022-06-22 22:04:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 (async () => {
 
   console.log(`Trophy And Crown Hack Safe! [ReEdit]
-By : ${chalk.bold('xDast#8745')} - Credit : @dkmpostor & @Eskey
+By : ${chalk.bold('RassGMS')} - Credit : @iChiro& @Lmao
 `);
 
   const auth = rs.question('Enter Authentication Code! : ');
